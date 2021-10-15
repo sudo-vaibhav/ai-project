aba838d81e009089027efaf33a0c4c816f5511e6
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-10-15 17:13:19 +0530

    added files

---
+++ backend/config/index.js
@@ -0,0 +1,3 @@
+exports.mongoDBUri = "mongodb+srv://educator-ai:vQk52MyZlw0zYUTW@cluster0.hqk1y.mongodb.net/edcuatorDb?retryWrites=true&w=majority"
+exports.userSearchUri = "http://845f-104-196-152-57.ngrok.io/"
+exports.testUri = "http://003b-35-194-167-237.ngrok.io/"
