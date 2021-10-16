0ef44a764002b24464a9c91499e52ec420cb3d2c
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-10-16 09:34:51 +0530

    changed link

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,3 +1,3 @@
 exports.mongoDBUri = "mongodb+srv://educator-ai:vQk52MyZlw0zYUTW@cluster0.hqk1y.mongodb.net/edcuatorDb?retryWrites=true&w=majority"
-exports.userSearchUri = "http://40f3-35-224-189-173.ngrok.io/"
-exports.testUri = "http://15ee-35-184-236-200.ngrok.io/"
+exports.userSearchUri = "http://d44a-34-73-253-77.ngrok.io/"
+exports.testUri = "http://b217-34-90-121-111.ngrok.io/"
